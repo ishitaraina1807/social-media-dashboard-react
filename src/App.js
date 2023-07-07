db97c475cee01cd0bea8df43aaf2ee53cac930e7
Mermaido <106395332+ishitaraina1807@users.noreply.github.com>
2023-07-07 20:47:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TopBar from './components/TopBar';
+// import Card from './components/Card';
+
+function App() {
+  return (
+    <>
+  <TopBar/>
+  {/* <Card/> */}
+  </>
+  );
+}
+
+export default App;
